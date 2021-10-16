68d07bd4bd3c7c939a05129ed09b6affce6b2d30
CharlieJhonSmith <charlie.alfrido@gmail.com>
2021-10-16 04:07:51 +0000

    add test

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum');
+const sum = require("./sum")
 
 test('adds parameters when both are supplied at once', () => {
   expect(false).toBe(true);
@@ -7,3 +7,54 @@ test('adds parameters when both are supplied at once', () => {
 test('adds 1 + 2 with parameters applied once per function call', () => {
   expect(false).toBe(true);
 });
+
+// @ponicode
+describe("sum", () => {
+    test("0", () => {
+        let callFunction = () => {
+            sum(-1, 10)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            sum(1, "bar")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            sum(0.0, -10)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            sum(1, 1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction = () => {
+            sum(1, -10)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction = () => {
+            sum(Infinity, Infinity)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
